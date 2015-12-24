1d310153c507d0238d863f738c61a0b6607c144d
saba-tandashvili <saba98721@gmail.com>
2015-12-24 18:01:21 +0400

    quiz

--- ProjectThree/pdo/mysql.php
+++ ProjectThree/pdo/mysql.php
@@ -6,7 +6,24 @@ try{
 	$conn = new PDO('mysql:host=127.0.0.1; dbname=sqllab' , "root", ""); //ჰოსტი, ბაზის სახელი, ლოგინი, პაროლი;
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // ჩაშენებული ფუნქცია რომელსაც შეუძლია შეცდომის სხვადასხვა ტიპის დაფიქსირება;
 
-	$conn->query("INSERT INTO rerwer");
+	//$title="shekitxvis satauri";
+	//$correct_answer=2;
+
+	$stmt = $conn->prepare("INSERT INTO questions(title, correct_answer) VALUES(:title, :correct_answer)");
+	$stmt->bindParam(':title', $title);
+	$stmt->bindParam(':correct_answer', $correct_answer);
+
+	$stmt->execute(array(
+		'title' => 'question title from array 1',
+		'correct_answer' => 1
+	));
+
+	$stmt->execute(array(
+		'title' => 'question title from array 2',
+		'correct_answer' => 1
+	));
+	
+
 }
 catch(PDOException $e){
 	file_put_contents('error_log.txt', $e->getMessage()); //შეცდომას თუ პოულობს try, ეს ფუნქცია ბეჭდავს; 
---
+++ ProjectThree/select.php
@@ -0,0 +1,18 @@
+<?php
+
+	try{
+		$conn = new PDO('mysql:host=127.0.0.1; dbname=sqllab' , "root", ""); 
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+
+		$stmt = $conn->query("SELECT * FROM question");
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+		while ($row = $stmt ->fetch()) {
+			print_r($row);
+		}
+	}
+	catch(PDOException $e){
+		echo $e->getMessage();
+	}
+
+?>
