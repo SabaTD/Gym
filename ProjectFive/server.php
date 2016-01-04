ff7dffcc9869947fad93d3bfe2a3973bacde211e
saba-tandashvili <saba98721@gmail.com>
2016-01-04 22:35:05 +0400

    #isnertintodatabase

--- ProjectFive/server.php
+++ ProjectFive/server.php
@@ -1,23 +1,21 @@
 <?php
-	if(isset($_POST['submit'])){
-		$servername = "loclahost";
-		$username = "username";
-		$password = "password";
-		$dbname = "mydb";
-
-		$conn = mysqli_connect($servername, $username, $password, $dbname);
-		if(!$conn){
-			die("Connection failed: ". mysqli_connect_error());
-		}
-
-		$sql = "INSERT INTO MyGuests(firstname, lastname, email) VALUES ($_POST[fname], $_POST[lname], $_POST[mail])";
-
-		if(mysqli_query($conn, $sql)) {
-			echo "New record created successfully";
-		}
-		else{
-			echo "Error" . $sql. "<br>" . mysqli_error($conn);
-		}
-		mysql_close($conn);
-	}
-?>
+ +	$servername = "localhost";
+ +	$username = "root";
+ +	$password = "";
+ +	$dbname = "mydb";
+  
+ +	$conn = mysqli_connect("localhost", "root", "", "mydb");
+ +	
+ +	$firstName = $_POST["fname"];
+ 	$lastName = $_POST["lname"];
+ +	$email = $_POST["mail"];
+  
+ -  //$sql = "INSERT INTO `MyGuests` (`firstname`, `lastname`, `email`) VALUES ('$firstName', '$lastName', '$email')";
+ +	$sql = "INSERT INTO `myguests` (`firstname`, `lastname`, `email`) VALUES ('$firstName', '$lastName', '$email')";
+  
+ 
+ +	mysqli_query($conn, $sql);
+ +	mysqli_close($conn);
+ 	header("location: form.php");
+ 	
+  ?>
