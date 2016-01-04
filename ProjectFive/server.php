04a5337e07e845cf113414bdab18e8a487a8833e
David Demetradze <davidemetradze@gmail.com>
2016-01-04 22:16:14 +0400

    Update server.php

--- ProjectFive/server.php
+++ ProjectFive/server.php
@@ -1,16 +1,12 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "mydb";
-
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	
+	$conn = mysqli_connect("localhost", "root", "", "mydb");
 	
 	$firstName = $_POST["fname"];
 	$lastName = $_POST["lname"];
 	$email = $_POST["mail"];
 
-	$sql = "INSERT INTO `MyGuests` (`firstname`, `lastname`, `email`) VALUES ('$firstName', '$lastName', '$email')";
+	$sql = "INSERT INTO `myguests` (`firstname`, `lastname`, `email`) VALUES ('$firstName', '$lastName', '$email')";
 
 	mysqli_query($conn, $sql);
 	mysql_close($conn);
