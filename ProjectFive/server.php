a85caeb1106ad4c353dc32ebf52adff26cce0677
David Demetradze <davidemetradze@gmail.com>
2016-01-04 22:03:11 +0400

    Update server.php

--- ProjectFive/server.php
+++ ProjectFive/server.php
@@ -1,27 +1,18 @@
 <?php
-	if(isset($_POST['submit'])){
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "mydb";
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "mydb";
 
-		$conn = mysqli_connect($servername, $username, $password, $dbname);
-		if(!$conn){
-			die("Connection failed: ". mysqli_connect_error());
-		}
-		
-		$firstName = $_POST["fname"];
-		$lastName = $_POST["lastName"];
-		$email = $_POST["email"];
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	
+	$firstName = $_POST["fname"];
+	$lastName = $_POST["lastName"];
+	$email = $_POST["email"];
 
-		$sql = "INSERT INTO `MyGuests` (`firstname`, `lastname`, `email`) VALUES ('$firstName', '$lastName', '$email')";
+	$sql = "INSERT INTO `MyGuests` (`firstname`, `lastname`, `email`) VALUES ('$firstName', '$lastName', '$email')";
 
-		if(mysqli_query($conn, $sql)) {
-			echo "New record created successfully";
-		}
-		else{
-			echo "Error" . $sql. "<br>" . mysqli_error($conn);
-		}
-		mysql_close($conn);
-	}
+	mysqli_query($conn, $sql);
+	mysql_close($conn);
+	
 ?>
