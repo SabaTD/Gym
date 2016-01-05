116af8d8678bec1ca7b1a5dde6b46fe439b42305
saba-tandashvili <saba98721@gmail.com>
2016-01-05 18:31:27 +0400

    #insert-select

--- ProjectFive/form.php
+++ ProjectFive/form.php
@@ -12,15 +12,41 @@
 	</form>
 
 	<?php 
+<<<<<<< HEAD
+<<<<<<< HEAD
+		//$servername = "localhost";
+ 		//$username = "root";
+ 		//$password = "";
+ 		//$dbname = "mydb";
+
+ 		$conn = mysqli_connect("localhost", "root", "", "mydb"); //რადგან აქ გიწერია ის ზედა აღარ უნდა, პირდაპირ აქ ჩაწერე
+ 		$sql = "SELECT * FROM `myguests`"; // ` სიმბოლო  გამოიყენე როცა  SQL ბრძანებაში იყენებ თეიბლის სახელს, თეიბლის  ველებს, 
+ 		$result = mysqli_query($conn, $sql);
+
+		//შეგიძლია პირდაპირ ასე დაწერო ->  $result= mysqli_query($conn, "select * FROM `myguests`");  
+		while($row = mysqli_fetch_array($result))
+		{
+ 			echo "FirstName:" . $row["firstname"]. "   ". "  ". "LastName:" . $row["lastname"]. "Email:". $row["email"]. "<br>";
+=======
 	
  		$conn = mysqli_connect("localhost", "root", "", "mydb"); //რადგან აქ გიწერია ის ზედა აღარ უნდა, პირდაპირ აქ ჩაწერე
  		$sql = "SELECT * FROM `myguests`"; // ` სიმბოლო  გამოიყენე როცა  SQL ბრძანებაში იყენებ თეიბლის სახელს, თეიბლის  ველებს, 
  		$result = mysqli_query($conn, $sql);
+=======
+	
+ 		$conn = mysqli_connect("localhost", "root", "", "mydb"); //რადგან აქ გიწერია ის ზედა აღარ უნდა, პირდაპირ აქ ჩაწერე
+ 		$sql = "SELECT * FROM `myguests`"; // ` სიმბოლო  გამოიყენე როცა  SQL ბრძანებაში იყენებ თეიბლის სახელს, თეიბლის  ველებს, 
+ 		$result = mysqli_query($conn, $sql);
+>>>>>>> origin/master
  		
  		//შეგიძლია პირდაპირ ასე დაწერო ->  $result= mysqli_query($conn, "select * FROM `myguests`");  
 		while($row = mysqli_fetch_array($result))
 		{
  			echo "FirstName:" . $row["fname"]. "LastName:" . $row["lname"]. "Email:". $row["mail"]. "<br>";
+<<<<<<< HEAD
+>>>>>>> origin/master
+=======
+>>>>>>> origin/master
  		}
  	
  		mysqli_close($conn);
---
+++ ProjectFive/server.php
@@ -0,0 +1,21 @@
+<?php
+ 	$servername = "localhost";
+ 	$username = "root";
+ 	$password = "";
+ 	$dbname = "mydb";
+  
+ 	$conn = mysqli_connect("localhost", "root", "", "mydb");
+ 	
+ 	$firstName = $_POST["fname"];
+ 	$lastName = $_POST["lname"];
+ 	$email = $_POST["mail"];
+  
+   //$sql = "INSERT INTO `MyGuests` (`firstname`, `lastname`, `email`) VALUES ('$firstName', '$lastName', '$email')";
+ 	$sql = "INSERT INTO `myguests` (`firstname`, `lastname`, `email`) VALUES ('$firstName', '$lastName', '$email')";
+  
+ 
+ 	mysqli_query($conn, $sql);
+ 	mysqli_close($conn);
+ 	header("location: form.php");
+
+  ?>
