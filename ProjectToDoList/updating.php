7feac4394eb68e32ad674613e6677c459b32058d
David Demetradze <davidemetradze@gmail.com>
2016-01-14 15:12:21 +0400

    Update updating.php

--- ProjectToDoList/updating.php
+++ ProjectToDoList/updating.php
@@ -1,58 +1,35 @@
 <?php
-	$servername = "localhost";
- 	$username = "root";
- 	$password = "";
- 	$dbname = "mydatabase";
- /* 
-	$conn = mysqli_connect("localhost", "root", "", "mydatabase");
-	
-	if (!$conn) {
-	    die("Connection failed: " . mysqli_connect_error());
-	}
-*/
-    $conn=mysql_connect("localhost", "root");
-    $db=mysql_select_db("mydatabase");
 	$id = $_GET['id'];
+
+	//როცა ასე წერ, აღარაა საჭირო ზემოთ განსაზღვრო. პირდაპირ აქ ჩაწერე
+	$con = mysqli_connect("localhost", "root", "", "mydatabase");
 	
-    $shedegi=mysql_query("select * from mylist where id='$id'");
+	if (!$con)
+	    die("Connection failed: " . mysqli_connect_error());
+	
+	//ქართული სახელები ცვლადზე არის  საშინელება ;დ
+	//არასოდეს არ გამოიყენო mysql_ ფუნქციები, გამოიყენე mysqli
+	//განსხვავება თუ გაინტერესებს დაგუგლე
+    	$result = mysqli_query($con, "SELECT * FROM `mylist` WHERE `id` = '$id'");
     
-   
-    $shedegi_array=mysql_fetch_array($shedegi);
-    $ragaca1=$shedegi_array['Title'];
-    $ragaca2=$shedegi_array['Description'];
-    $ragaca3=$shedegi_array['Date'];
-	//$sql = "SELECT mylist SET Title='$satauri', Description = '$agwera', Date = '$tarigi' where id='$id'";
-
-
-	/*if (mysqli_query($conn, $sql)) {
-	    echo "Record updated successfully";
-	} else {
-	    echo "Error updating record: " . mysqli_error($conn);
-	}
-	*/
-	//mysqli_close($conn);
-	
-	
+	$row = mysqli_fetch_array($result);
+		$title = $row['Title'];
+    		$description = $row['Description'];
+    		$date = $row['Date'];
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
-</head>
-<body>
-<form class="form-group" action="update.php" method="post">
-
-		Title:<input type="text" class="form-control" name="satauri" value="<?php echo $ragaca1; ?>"></br>
-		Description:<textarea class="form-control" rows="5" name="agwera"><?php echo $ragaca2; ?></textarea></br>
-		<!-- Adding Date:<input type="text" id="tarigi" class="form-control" name="tarigi" value=" <?php echo $ragaca3 ?>"  readonly></br> -->
-		<input type="text" value="<?php echo $id ?>" name="id" readonly />
-		<input type="submit"  class="btn btn-primary" value="Update" name="submit">
-
-</form> 
-
-
-
-</body>
+	<head>
+		<title></title>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+	</head>
+	<body>
+		<form class="form-group" action="update.php" method="post">
+			<label>Title:</label></br><input type="text" class="form-control" name="satauri" value="<?php echo $ragaca1; ?>"></br>
+			<label>Description:</label><textarea class="form-control" rows="5" name="agwera"><?php echo $ragaca2; ?></textarea></br>
+			<input type="text" value="<?php echo $id ?>" name="id" readonly />
+			<input type="submit"  class="btn btn-primary" value="Update" name="submit" />
+		</form> 
+	</body>
 </html>
