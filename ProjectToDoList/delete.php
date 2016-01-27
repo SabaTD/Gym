ab3871edc97f31aee0a66b18834e066e284f5433
David Demetradze <davidemetradze@gmail.com>
2016-01-27 10:03:12 +0400

    Update delete.php

--- ProjectToDoList/delete.php
+++ ProjectToDoList/delete.php
@@ -2,20 +2,14 @@
  	$conn = mysqli_connect("localhost", "root", "", "mydatabase");
 
 	if (!$conn) 
-	{
 	    die("Connection failed: " . mysqli_connect_error());
-	}
 	
 	$sql = "DELETE FROM mylist WHERE id='$id'";
 
 	if (mysqli_query($conn, $sql)) 
-	{
 	    echo "Record deleted successfully";
-	} 
 	else 
-	{
 	    echo "Error deleting record: " . mysqli_error($conn);
-	}
 
 	mysqli_close($conn);
 	header("location: form.php");
